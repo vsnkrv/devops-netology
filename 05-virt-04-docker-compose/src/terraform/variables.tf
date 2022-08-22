1dc6dce5de1d255053ad3121d91f439429f8ef09
Rustam Vasenkin <wrw@list.ru>
2022-08-23 04:38:24 +0500

    05-virt-04-docker-compose

---
+++ 05-virt-04-docker-compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1geph3c0f6qus8lej91"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gg0jj52l7adehme4ff"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8d1l0smu3pvoiae133"
+}
